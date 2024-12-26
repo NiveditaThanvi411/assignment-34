4a743b6be193c4b2918ccb0f7d6e0d0ae527d022
NiveditaThanvi411 <niveditathanvi@gmail.com>
2024-12-26 18:14:51 +0530

    inital commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function loadStudents() {
+    const studentsContainer = document.getElementById("students-list");
+    studentsContainer.innerHTML = "";
+  
+    for (let i = 0; i < students.length; i++) {
+      studentsContainer.innerHTML += <div class="students-card">${i + 1}. ${students[i]}</div>;
+    }
+  }
+  
+  loadStudents();
+  
+  const studentInput = document.getElementById("student-name");
+  
+  function addStudent() {
+    students.push(studentInput.value);
+    studentInput.value = "";
+    loadStudents();
+  }
+  
+  function removeStudent() {
+    const studentIndex = students.indexOf(studentInput.value);
+    studentInput.value = "";
+    students.splice(studentIndex, 1);
+    loadStudents();
+  }
+  
+  
